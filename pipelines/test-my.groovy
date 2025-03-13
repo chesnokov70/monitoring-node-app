5a2ec86b7c6d9e92a04a61c1b5bf47a5da8412b4
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-03-12 21:54:21 -0400

    added pipelines2

---
+++ pipelines/test-my.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any 
+    parameters {
+        choice(name: 'playbook', choices: ["install_node_app.yml"])
+        string(name: 'host', defaultValue: 'iP', trim: true)
+        string(name: 'branch', defaultValue: 'main', description: 'Git branch to checkout') // Replaced 'gitParameter' with 'string'
+        booleanParam(name: 'dryrun', defaultValue: true)
+    }
+    environment {
+        GIT_URL = 'git@github.com:chesnokov70/monitoring-node-app.git'  // Defined missing variable
+    }
+    stages {
+        stage('Checkout repo') {
+            steps {
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: "*/${params.branch}"]], 
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [], 
+                    submoduleCfg: [], 
+                    userRemoteConfigs: [[
+                        credentialsId: 'ssh_github_access_key', 
+                        url: "${env.GIT_URL}"
+                    ]]
+                ])
+            }
+        }
+      
+        stage('DryRun') {
+            when {
+                expression { params.dryrun }
+            }
+            steps {
+                sh """
+                  export ANSIBLE_HOST_KEY_CHECKING=False
+                  ansible-playbook -i \"${params.host},\" ${params.playbook} --check
+                """
+            }
+        }
+
+        stage('Apply') {
+            when {
+                expression { !params.dryrun }
+            }
+            steps {
+                sh """
+                  export ANSIBLE_HOST_KEY_CHECKING=False
+                  ansible-playbook -i \"${params.host},\" ${params.playbook}
+                """
+            }
+        }
+    }
+}
